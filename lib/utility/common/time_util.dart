b430bbd3cbd16e8d6d4fa3b8b1e10b2b83ed094b
Alpha Yu <wtuyp@163.com>
2025-12-29 14:04:09 +0800

    日期相关工具更新

--- lib/utility/common/time_util.dart
+++ lib/utility/common/time_util.dart
@@ -1,17 +1,17 @@
 
-import 'package:app/utility/extention/date_time_extension.dart';
-import 'package:intl/intl.dart';
+import 'package:app/utility/extension/date_time_extension.dart';
 
+/// 时间工具类
 class TimeUtil {
 
-  // 分钟转时间显示，如 100 -> 01:40
+  /// 分钟转时间显示，如 100 -> 01:40
   static String timeStringFromMinutes(int minutes) {
     int hours = minutes ~/ 60;
     int remainingMinutes = minutes % 60;
     return '${hours.toString().padLeft(2, '0')}:${remainingMinutes.toString().padLeft(2, '0')}';
   }
 
-  // 时间显示转分钟，如 01:40 -> 100
+  /// 时间显示转分钟，如 01:40 -> 100
   static int minutesFromTimeString(String timeString) {
     List<String> parts = timeString.split(':');
     if (parts.length != 2) {
@@ -21,66 +21,6 @@ class TimeUtil {
     int hours = int.tryParse(parts[0]) ?? 0;
     int minutes = int.tryParse(parts[1]) ?? 0;
     return hours * 60 + minutes;
-  }
-
-  /// 获取指定日期0点的DateTime对象
-  static DateTime getMidnightOf(DateTime date) {
-    return DateTime(date.year, date.month, date.day);
-  }
-
-  /// 获取当天0点的DateTime对象
-  static DateTime getMidnight([DateTime? referenceDate]) {
-    final now = referenceDate ?? DateTime.now();
-    return DateTime(now.year, now.month, now.day);
-  }
-
-  /// 获取当天0点的字符串表示（格式：yyyy-MM-dd HH:mm:ss）
-  static String getMidnightString() {
-    final midnight = getMidnight();
-    return getDateTimeString(midnight);
-  }
-
-  /// 获取指定日期的字符串表示（格式：yyyy-MM-dd HH:mm:ss）
-  static String getDateTimeString(DateTime dateTime) {
-    return '${dateTime.year}-${dateTime.month.toString().padLeft(2, '0')}-${dateTime.day.toString().padLeft(2, '0')} ${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}:${dateTime.second.toString().padLeft(2, '0')}';
-  }
-
-  /// 获取指定日期的字符串表示（格式：yyyy-MM-dd）
-  static String getDateString(DateTime dateTime) {
-    return '${dateTime.year}-${dateTime.month.toString().padLeft(2, '0')}-${dateTime.day.toString().padLeft(2, '0')}';
-  }
-
-  /// 获取指定日期的字符串表示（格式：yyyyMMdd）
-  static String getDateString_yyyyMMdd(DateTime dateTime) {
-    return '${dateTime.year}${dateTime.month.toString().padLeft(2, '0')}${dateTime.day.toString().padLeft(2, '0')}';
-  }
-  /// 获取指定日期的字符串表示（格式：yyyy-MM）
-  static String getDateString_yyyyMM(DateTime dateTime) {
-    return '${dateTime.year}-${dateTime.month.toString().padLeft(2, '0')}';
-  }
-  /// 获取指定日期的字符串表示（格式：HH:mm）
-  static String getDateString_HHmm(DateTime dateTime) {
-    return '${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}';
-  }
-
-  /// 获取指定日期的字符串表示（格式：MM月dd日 HH:mm）
-  static String getDateString_MMddHHmm_cn(DateTime dateTime) {
-    return '${dateTime.month.toString().padLeft(2, '0')}月${dateTime.day.toString().padLeft(2, '0')}日 ${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}';
-  }
-
-  /// 获取指定日期的字符串表示（格式：MM月dd日）
-  static String getDateString_MMdd_cn(DateTime dateTime) {
-    return '${dateTime.month.toString().padLeft(2, '0')}月${dateTime.day.toString().padLeft(2, '0')}日';
-  }
-
-  /// 将毫秒数转换为DateTime对象
-  static DateTime getDateTimeFromMilliseconds(int milliseconds) {
-    return DateTime.fromMillisecondsSinceEpoch(milliseconds);
-  }
-
-  /// 获取指定时间的整点时间戳（单位：秒）
-  static int getHourPointTimestamp(DateTime dateTime) {
-    return DateTime(dateTime.year, dateTime.month, dateTime.day, dateTime.hour).millisecondsSinceEpoch ~/ 1000;
   }
 
   /// 格式化时间为相对时间（例如：1天前、1小时前等）
---
+++ lib/utility/extension/date_time_extension.dart
@@ -0,0 +1,117 @@
+
+extension DateTimeExtension on DateTime {
+
+  /// 0点的时间
+  DateTime get atZero => DateTime(year, month, day);
+
+  /// 周第一天 （firstDayOfWeek 周起始日）
+  DateTime firstDayOfWeek({int firstWeekDay = DateTime.monday}) {
+    assert(firstWeekDay >= 1 && firstWeekDay <= 7);
+    int offset = (weekday - firstWeekDay) % DateTime.daysPerWeek;
+    return subtract(Duration(days: offset));
+  }
+
+  /// 周最后一天 firstDayOfWeek 周起始日）
+  DateTime lastDayOfWeek({int firstWeekDay = DateTime.monday}) {
+    DateTime firstDay = firstDayOfWeek(firstWeekDay: firstWeekDay);
+    return firstDay.add(Duration(days: DateTime.daysPerWeek - 1));
+  }
+
+  /// 月第一天
+  DateTime firstDayOfMonth() => DateTime(year, month, 1, hour, minute, millisecond, microsecond);
+  /// 月最后一天
+  DateTime lastDayOfMonth() => DateTime(year, month + 1, 0, hour, minute, millisecond, microsecond);
+
+  /// 年第一天
+  DateTime firstDayOfYear() => DateTime(year, 1, 1, hour, minute, millisecond, microsecond);
+  /// 年最后一天
+  DateTime lastDayOfYear() => DateTime(year, 12, 31, hour, minute, millisecond, microsecond);
+
+  /// 是否同一天
+  bool isSameDay(DateTime date) {
+    return year == date.year && month == date.month && day == date.day;
+  }
+
+  /// 转换为相对时间字符串（例如：1天前、1小时前等）
+  String toRelativeTimeString() {
+    final difference = DateTime.now().difference(this);
+
+    if (difference.inDays > 30) {
+      return '${(difference.inDays / 30).floor()}月前';
+    } else if (difference.inDays > 0) {
+      return '${difference.inDays}天前';
+    } else if (difference.inHours > 0) {
+      return '${difference.inHours}小时前';
+    } else if (difference.inMinutes > 0) {
+      return '${difference.inMinutes}分钟前';
+    } else {
+      return '刚刚';
+    }
+  }
+
+}
+
+/// 时间戳（秒）
+extension DateTimeTimestamp on DateTime {
+
+  /// 时间戳（秒）
+  int get secondsSinceEpoch => millisecondsSinceEpoch ~/ 1000;
+
+  /// 分钟所对应的时间戳（秒）
+  int getMinuteTimestamp() {
+    DateTime date = DateTime(year, month, day, hour, minute);
+    return date.secondsSinceEpoch;
+  }
+
+  /// 整点所对应的时间戳（秒）
+  int getHourTimestamp() {
+    DateTime date = DateTime(year, month, day, hour);
+    return date.secondsSinceEpoch;
+  }
+}
+
+/// 时间格式化 (因为使用 format 比较耗时)
+extension DateTimeFormat on DateTime {
+
+  /// 转成 yyyy-MM-dd HH:mm:ss 格式的字符串
+  String toDateTimeString() {
+    final String year = this.year.toString().padLeft(4, '0');
+    final String month = this.month.toString().padLeft(2, '0');
+    final String day = this.day.toString().padLeft(2, '0');
+    final String hour = this.hour.toString().padLeft(2, '0');
+    final String minute = this.minute.toString().padLeft(2, '0');
+    final String second = this.second.toString().padLeft(2, '0');
+    return '$year-$month-$day $hour:$minute:$second';
+  }
+
+  /// 转成 yyyy-MM-dd 格式的字符串
+  String toDateString() {
+    final String year = this.year.toString().padLeft(4, '0');
+    final String month = this.month.toString().padLeft(2, '0');
+    final String day = this.day.toString().padLeft(2, '0');
+    return '$year-$month-$day';
+  }
+
+  /// 转成 yyyy-MM 格式的字符串
+  String toMonthString() {
+    final String year = this.year.toString().padLeft(4, '0');
+    final String month = this.month.toString().padLeft(2, '0');
+    return '$year-$month';
+  }
+
+  /// 转成 HH:mm 格式的字符串
+  String toTimeString() {
+    final String hour = this.hour.toString().padLeft(2, '0');
+    final String minute = this.minute.toString().padLeft(2, '0');
+    return '$hour:$minute';
+  }
+
+  /// 转成 yyyyMMdd 格式的字符串
+  String toCompactDateString() {
+    final String year = this.year.toString().padLeft(4, '0');
+    final String month = this.month.toString().padLeft(2, '0');
+    final String day = this.day.toString().padLeft(2, '0');
+    return '$year$month$day';
+  }
+}
+
